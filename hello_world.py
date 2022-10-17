7677fda680ddd769d8d2766d4d9b3417e3e4b3ce
Imad Ottallah <imadotta@gmail.com>
2022-10-16 20:28:58 -0500

    done

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print("Howdy Y'all!!")
+
+animals = [ "Triceratops", "Gorilla", "Corgi", "Toucan"]
+
+for animal in animals:
+  if len(animal) == 5:
+    print(animal)
+
+greeting = input("Pick your greeting: \n A. Howdy B. hello C. OK' [A/B/C]? : ")
+if greeting == "A": 
+  print ("Yes")
+elif greeting == "B":
+  print("no")
+elif greeting == "C":
+  print("PK")
